43c11760126306a2235ce5e1f206f7481db9f7f3
kuaifang <geofkwai@gmail.com>
2016-01-02 17:22:08 -0500

    SSRS functions

---
+++ SSRS.py
@@ -0,0 +1,70 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+def clusterplot(data,lable,center=[]):
+    nclass=len(np.unique(lable))
+    nind_class=np.bincount(lable)
+    sortind=np.argsort(nind_class)[::-1]
+    if nclass>10:
+        n=10
+    else:
+        n=nclass
+    f, axarr = plt.subplots(int(np.ceil(n/2)), 2)
+    for i in range(n):
+        ind=sortind[i]
+        pj=int(np.ceil(i/2))
+        pi=int(i%2)
+        axarr[pj, pi].boxplot(data[lable==ind])
+        axarr[pj, pi].set_title('cluster %s, nind=%s'%(ind+1,nind_class[ind]))
+        axarr[pj, pi].set_ylim([-1,1])
+        if len(center)>0:
+            axarr[pj, pi].plot(center[ind,],'*-r')
+
+### training
+def ClusterLearn_cross(XXn,T,nfold,model):
+    indran=range(len(T))
+    random.shuffle(indran)
+    indran=np.asarray(indran)
+    ngroup=int(np.floor(len(T)/nfold))
+    Tp=np.zeros((len(T),),int)
+
+    for i in range(10):
+        if i==nfold-1:
+            ind=indran[range(i*ngroup,len(indran),1)]
+        else:
+            ind=indran[range(i*ngroup,(i+1)*ngroup-1,1)]
+        X1=XXn[ind]
+        T1=T[ind]
+        X0=np.delete(XXn,ind,0)
+        T0=np.delete(T,ind)
+        model = model.fit(X0, T0)
+        Tp[ind]=model.predict(X1)
+    return Tp
+
+
+def plotErrorMap(T,Tp):
+    #error map and performance plot
+    nclass=len(np.unique(T))
+    errormap=np.zeros((nclass,nclass),int)
+    for i in range(len(T)):
+        errormap[Tp[i],T[i]]=errormap[Tp[i],T[i]]+1
+
+    plt.matshow(errormap)
+    plt.ylabel("Predict")
+    plt.xlabel("Truth")
+    for i in range(nclass):
+        for j in range(nclass):
+            plt.text(j,i,errormap[j,i], va='center', ha='center',size=20)
+
+    perf1=np.zeros((nclass),float)
+    perf2=np.zeros((nclass),float)
+    for i in range(nclass):
+        perf1[i]=float(errormap[i,i])/float(np.sum(errormap[i,:]))
+        perf2[i]=float(errormap[i,i])/float(np.sum(errormap[:,i]))
+    plt.yticks(range(nclass),["%.3f"%i for i in perf1])
+    plt.xticks(range(nclass),["%.3f"%i for i in perf2])
+    ncorrect=np.sum(errormap[range(nclass),range(nclass)])
+    accu=float(ncorrect)/float(len(T))
+    plt.title("total accuracy %.3f"%accu)
