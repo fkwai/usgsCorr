acc2d4ae8862e028fd6fd0a3ae83544a02e7e554
kuaifang <geofkwai@gmail.com>
2016-03-20 17:10:01 -0400

    update load data to SSRS_script.py

--- SSRS_script.py
+++ SSRS_script.py
@@ -26,23 +26,12 @@ from sklearn import neural_network
 
 ### read data
 UCdir = "Y:\Kuai\USGSCorr\\"
-UCfile = UCdir + "usgsCorr_maxmin.mat"
-Datafile = UCdir + "dataset2.mat"
+UCfile=UCdir+"usgsCorr2.mat"
+Datafile=UCdir+"dataset2.mat"
 mat = sio.loadmat(UCfile)
-UCData = mat['Corr_maxmin']
-id1 = mat['ID']
-id1.reshape(len(id1), )  # change to 1d array in case of further trouble
+UCData=mat['Corr_maxmin']
 mat = sio.loadmat(Datafile)
-AttrData = mat['dataset']
-id2 = mat['ID']
-id2.reshape(len(id2), )  # change to 1d array in case of further trouble
-
-id = np.intersect1d(id1, id2)
-ind1 = np.zeros((len(id),), int)
-ind2 = np.zeros((len(id),), int)
-for i in range(len(id)):
-    ind1[i] = (id[i] == id1).nonzero()[0]
-    ind2[i] = (id[i] == id2).nonzero()[0]
+AttrData=mat['dataset']
 
 # ### read departure data
 # filename="Y:\Kuai\USGSCorr\dataset_departure.mat"
